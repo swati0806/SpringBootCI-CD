c8eae585c79fe2f12587c09c8f79f12fc4b3a899
swati0806 <73926313+swati0806@users.noreply.github.com>
2023-05-24 12:07:38 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Hi Friends Latest Devops Projects Uploaded Daily Based . LiKe & Share this video & also Subscribe";
+		return "Hi, this project  is automation using maven, kubernetes cluster, jenkins and docker";
 	}
 
 	public static void main(String[] args) {
